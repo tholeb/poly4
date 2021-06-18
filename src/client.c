e97c975e779802fc3368597a27431eea9132645a
john-batch <70102999+john-batch@users.noreply.github.com>
2021-06-18 10:19:41 +0200

    More comments

--- src/client.c
+++ src/client.c
@@ -11,6 +11,7 @@
 
 int main(void)
 {
+	// créé un socket (PF_INET = IPv4, SOCK_STREAM = TCP)
 	int sock = socket(PF_INET, SOCK_STREAM, 0);
 
 	perror("socket");
@@ -20,16 +21,19 @@ int main(void)
 		return 1;
 	}
 
+	// adresse du serveur (127.0.0.1:8000)
 	struct sockaddr_in addr;
 
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_port = htons(8000);
 
+	// connecte le client au serveur
 	int err = connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr));
 
 	perror("connect");
 
+	// au cas d'échec de connexion, on ferme le socket et on sort.
 	if (err)
 	{
 		close(sock);
@@ -37,13 +41,19 @@ int main(void)
 		return 1;
 	}
 
+	// chaque message du serveur vers le client aura le format suivant :
+	// le premier octet indique le type du message
+	// le second octet est un numéro de colonne (ou rien du tout selon le type de message)
 	uint8_t msg[2];
 
+	// numéro du joueur qui joue actuellement (utile pour savoir quel pion afficher).
 	int turn = 0;
 
 	do {
+		// affichage de la grille
 		afficher_grille();
 
+		// réception du message
 		recv(sock, msg, 2, 0);
 
 		switch (msg[0])
@@ -52,12 +62,16 @@ int main(void)
 			{
 				int col = demander_coup();
 
+				// les messages du client vers le serveur ne sont toujours
+				// constitués que d'un seul octet (le numéro de colonne à jouer)
 				send(sock, &col, 1, 0);
 			}
 			break;
 			case INDIQUER_COUP:
+			// ajoute le coup joué à la grille
 			ajouter_pion(turn ? PION_J1 : PION_J2, msg[1]);
 
+			// passe le tour au joueur qui ne jouait pas sur ce tour
 			turn = !turn;
 			break;
 			case FIN_DE_PARTIE:
@@ -75,6 +89,7 @@ int main(void)
 	}
 	while (msg[0] != FIN_DE_PARTIE);
 
+	// ferme le socket
 	close(sock);
 
 	return 0;
